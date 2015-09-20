0402f9b0a74123a61d466d32d3c367aa76d2be2d
Mike Liu <mikesholiu@gmail.com>
2015-09-19 21:10:52 -0700

    css template, removed instafeed because not relevant

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,21 +15,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-$(function() {
- 
-    //Set up instafeed
-    var feed = new Instafeed({
-        clientId: 'd666f96ba0444e8eb8610c88e47ff417',
-        target: 'instafeed',
-        get: 'popular',
-        tagName: 'none',
-        links: true,
-        limit: 8,
-        sortBy: 'most-recent',
-        resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
-    });
-    feed.run();
- 
-});
