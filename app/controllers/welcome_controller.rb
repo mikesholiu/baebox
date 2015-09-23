8b0fe4b6e95d35e0ff580e8aed4da6fe27843477
Mike Liu <mikesholiu@gmail.com>
2015-09-22 23:23:48 -0700

    template grid works, instagram config file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @recent = Instagram.user_recent_media("225240798", count: 8)
   end
 end
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,8 @@
+require 'instagram'
+
+Instagram.configure do |config|
+
+  config.client_id = "d666f96ba0444e8eb8610c88e47ff417"
+  config.access_token = "5053698.d666f96.16fb9831e5ce4fe59d980f16e7264adc"
+
+end
