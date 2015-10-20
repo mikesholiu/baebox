d6b781507b219707db20396f4fa02b9a6e9d6802
Mike Liu <mikesholiu@gmail.com>
2015-10-20 01:39:28 -0700

    home page displays featured items

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @recent = Instagram.user_recent_media("225240798", count: 20)
+    #@recent = Instagram.user_recent_media("225240798", count: 20)
+    @featured = Product.where(featured: true)
   end
-
   def omniauth_failure
     redirect_to root_path
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-  validates :price, numericality: true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0}
 end
