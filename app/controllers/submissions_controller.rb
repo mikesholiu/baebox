3d2e6b8cc9b3a311d502b045f934e47498cf73df
Mike Liu <mikesholiu@gmail.com>
2015-12-03 12:12:02 -0800

    submissions coffeescript

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -8,8 +8,8 @@ class SubmissionsController < ApplicationController
 end
 
 
-# After "Get pictures button" clicked...allof this happens in Javascript:
-# Read the value from the textbot for username
+# After "Get pictures button" clicked...all of this happens in Javascript:
+# Read the value from the textbox for username
 # $.ajax to Instagram to get images
 # parse the image urls from the ajax response
 # dynamically append radio buttons to the form for each image. you should write these in pure HTML
